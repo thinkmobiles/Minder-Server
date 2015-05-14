d95de2c7c59fee6f8ffdad7866f8115dde6c5222
nazarovits <istvan.nazarovits@gmail.com>
2015-05-14 10:38:10 +0200

    stripe public key

--- public/js/config/config.js
+++ public/js/config/config.js
@@ -2,8 +2,8 @@ define([],
     function () {
         var config = {
             recaptchaSyteKay: '6Lfy2QUTAAAAAP194SXgS0p_GNLxlGjffG7TLCIr',
-            //strypePublicKay: 'pk_test_8rIPyYpa3GHtdP7Ii77npqVW',
-            strypePublicKay: 'pk_test_IRQmWUyJVkJPNqVfqa9Y6U1y' //istvan
+            strypePublicKay: 'pk_test_8rIPyYpa3GHtdP7Ii77npqVW'
+            //strypePublicKay: 'pk_test_IRQmWUyJVkJPNqVfqa9Y6U1y' //istvan
         };
         return config
     });
